07af5e8e1e02c9a63d368e117ca803f1f3732f65
brendank310 <bkerrig1@binghamton.edu>
2016-06-24 15:00:20 -0400

    Revert "updates for new build system"

--- vcpu_factory_msr_bitmap/src/vcpu_factory_msr_bitmap.cpp
+++ vcpu_factory_msr_bitmap/src/vcpu_factory_msr_bitmap.cpp
@@ -21,17 +21,30 @@
 
 #include <vcpu/vcpu_factory.h>
 #include <vcpu/vcpu_intel_x64.h>
-#include <vmcs_msr_bitmap/vmcs_intel_x64_msr_bitmap.h>
+#include <vmcs_intel_x64_msr_bitmap.h>
 #include <exit_handler_msr_bitmap/exit_handler_msr_bitmap.h>
 
 std::shared_ptr<vcpu>
 vcpu_factory::make_vcpu(int64_t vcpuid)
 {
-    auto vmcs = std::make_shared<vmcs_intel_x64_msr_bitmap>();
+    // The vCPU Factory is used by the vCPU Manager to create vCPUs. This not
+    // only provides a hook for unit testing (the classic factory pattern), but
+    // also provides a clever place to hook in custom functionality above and
+    // beyond what bareflank provides by default.
+
+    // In this example, we are providing a custom exit handler that does not
+    // have any MSR emulation included.
     auto exit_handler = std::make_shared<exit_handler_msr_bitmap>();
 
-    // Return a vCPU with our custom objects instead of the defaults which
-    // are represented by the null pointers.
+    // Provide a custom VMCS that initializes the MSR bitmap
+    auto vmcs = std::make_shared<vmcs_intel_x64_msr_bitmap>();
+
+    // Now that we have our custom exit handler, we can provide it to the
+    // vCPU that bareflank provides. Note that you could create your own vCPU
+    // if you need to, but in general, the existing vCPU should work for most
+    // cases as it allows you to override each of the classes that are required.
+    // In all cases here, if nullptr is provided, the code under the hood will
+    // create the class for you using the defaults.
     return std::make_shared<vcpu_intel_x64>(vcpuid,
                                             nullptr,
                                             nullptr,
