71ab692dea35aa9d954d430e686427c960a7e346
Manohar Reddy <201451024@iiitvadodara.ac.in>
2018-03-10 21:54:28 +0530

    Update to new build system
    Update msr_bitmap example to new build system
    
    Signed-off-by: "Manohar Reddy" <"201451024@iiitvadodara.ac.in">

--- src/vcpu_factory_msr_bitmap.cpp
+++ src/vcpu_factory_msr_bitmap.cpp
@@ -19,22 +19,45 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-#include <vcpu/vcpu_factory.h>
-#include <vcpu/vcpu_intel_x64.h>
-#include <vmcs_msr_bitmap/vmcs_msr_bitmap.h>
+#include <bfvmm/memory_manager/memory_manager.h>
+#include <bfvmm/vcpu/vcpu_factory.h>
+#include <bfvmm/vcpu/arch/intel_x64/vcpu.h>
 
-std::unique_ptr<vcpu>
-vcpu_factory::make_vcpu(vcpuid::type vcpuid, user_data *data)
+using namespace ::intel_x64::vmcs;
+
+namespace example
 {
-    auto &&my_vmcs = std::make_unique<vmcs_msr_bitmap>();
+namespace intel_x64
+{
 
-    (void) data;
-    return std::make_unique<vcpu_intel_x64>(
-               vcpuid,
-               nullptr,                         // default debug_ring
-               nullptr,                         // default vmxon
-               std::move(my_vmcs),
-               nullptr,                         // default exit handler
-               nullptr,                         // default vmm_state
-               nullptr);                        // default guest_state
+class vcpu : public bfvmm::intel_x64::vcpu
+{
+private:
+    std::unique_ptr<uint8_t[]> m_msr_bitmap{std::make_unique<uint8_t[]>(x64::page_size)};
+	
+public:
+
+    vcpu(vcpuid::type id) :
+        bfvmm::intel_x64::vcpu{id}
+    {
+        address_of_msr_bitmap::set(g_mm->virtptr_to_physint(m_msr_bitmap.get()));
+        primary_processor_based_vm_execution_controls::use_msr_bitmap::enable();
+    }
+
+    ~vcpu() override = default;  
+};
+
+}
+}
+
+namespace bfvmm
+{
+
+WEAK_SYM std::unique_ptr<vcpu>
+vcpu_factory::make_vcpu(vcpuid::type vcpuid, bfobject *obj)
+{
+    bfignored(obj);
+    return std::make_unique<example::intel_x64::vcpu>(vcpuid);
+}
+
 }
